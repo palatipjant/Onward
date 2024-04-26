bf9e1fd19d67214ce9efdd924ba5ef3a8712eeb8
palatipjant <palatipjant@outlook.com>
2024-04-26 10:07:45 +0700

    Initial Commit

---
+++ Onward/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Onward
+//
+//  Created by Palatip Jantawong on 26/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Onward/OnwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnwardApp.swift
+//  Onward
+//
+//  Created by Palatip Jantawong on 26/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct OnwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
